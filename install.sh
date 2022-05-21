9b77980fa41cf4ac8a44eb6d78acc2dc4b49f515
supergnaw <loren.supernaw@gmail.com>
2022-05-21 08:36:15 -0500

    Create install.sh
    Initial testing on the main branch because we only do testing on prod

---
+++ install.sh
@@ -0,0 +1,85 @@
+if [ `whoai` == "root" ]; then
+    if [ `whoai` == "root" ]; then
+        echo "Please run this script as root or using sudo for the initial install"
+        exit
+    fi
+
+    # Check for fullstack user
+    if [ 0 == `getent passwd fullstack | wc -l` ]; then
+        # Check if fullstack user password was provided
+        if [ ! ${1} ]; then
+            echo "Please provide a password for new user account: fullstack"
+            exit
+        else
+            # Add fullstack user
+            useradd -m -s /bin/bash fullstack
+
+            # Set fullstack user password
+            echo "fullstack:${1}" | chpasswd
+        fi
+    fi
+
+    # Clone the repository
+    if [ ! -d "/opt/Full-Stack-Minecraft" ]; then
+        git clone https://github.com/supergnaw/Full-Stack-Minecraft.git /opt/Full-Stack-Minecraft
+    else
+        cd "/opt/Full-Stack-Minecraft"
+        git fetch
+        UPDATES=`git diff --shortstat origin | cut -d " " -f 2`
+        [[ $UPDATES =~ '([1-9]+|[1-9][0-9]+)' ]]
+        if [ $UPDATES == $BASH_REMACH[1] ]; then
+            git pull
+        else
+            echo ""
+        fi
+    fi
+	# Create log directory and associated logs
+	if [ ! -d "/var/log/fullstack" ]; then
+		mkdir "/var/log/fullstack"
+	fi
+    touch "/var/log/fullstack/install.log"
+
+    # Permissions
+    chown -R fullstack:fullstack "/opt/Full-Stack-Minecraft"
+    chmod -R 755 "/opt/Full-Stack-Minecraft"
+    chown -R fullstack:fullstack "/var/log/fullstack/update.log"
+    chmod -R 755 "/var/log/fullstack/update.log"
+
+    # Create cron job for automatic updates
+    CRONTAB=/var/spool/cron/crontabs/fullstack
+    if [[ -f "${CRONTAB}" ]]; then
+        rm "${CRONTAB}"
+    fi
+    touch "${CRONTAB}"
+    echo "5 * * * * bash /opt/Full-Stack-Minecraft/install.sh" | tee -a "${CRONTAB}"
+    chmod 600 "${CRONTAB}"
+    chown fullstack:fullstack "${CRONTAB}"
+
+    exit
+else
+    if [ $USER == "fullstack" ]; then
+        # Account check
+        if [ ! -d "/opt/Full-Stack-Minecraft" ]; then
+            echo "Please run this script as root or using sudo for the initial install"
+            exit
+        fi
+
+        # Check for repository updates
+        cd "/opt/Full-Stack-Minecraft"
+        git fetch
+        UPDATES=`git diff --shortstat origin | cut -d " " -f 2`
+        [[ $UPDATES =~ '([1-9]+|[1-9][0-9]+)' ]]
+        if [ $UPDATES == $BASH_REMACH[1] ]; then
+            # If updates exist, pull them
+            echo "$(date +"%F %T"): ${UPDATES} found, updating..." | tee -a "/var/log/fullstack/update.log"
+    		git pull
+        else
+            # No updates were found
+            echo "$(date +"%F %T"): ${UPDATES} found." | tee -a "/var/log/fullstack/update.log"
+        fi
+
+        exit
+    fi
+fi
+
+echo "Please run this script as root for the initial install or to repair an existing install, or as user fullstack to check for updates."
