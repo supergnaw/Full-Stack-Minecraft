12f0867358a72c99ae964f5dc07eacbe41d26824
supergnaw <loren.supernaw@gmail.com>
2022-05-29 11:50:14 -0500

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,8 +1,6 @@
 # Start Minecraft server
-USER=`whoami`
-
-if [ ! screen -ls | grep -Pq "[\d]+\.${USER}" ]; then
+if [ ! screen -ls | grep -Pq "[\d]+\.$(whoami)_server" ]; then
 	SERVER_VERSION="1.18.2"
-	cd "/home/${USER}/server"
-	screen -dmS ${USER} java -Xmx2G -Xms2G -jar mcserver.jar nogui
+	cd "/home/$(whoami)/server"
+	screen -dmS "$(whoami)_server" java -Xmx2G -Xms2G -jar mcserver.jar nogui
 fi
