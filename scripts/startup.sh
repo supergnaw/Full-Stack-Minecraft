3705cc880f37ceb4ac1bcb4a367ec902dfb65316
supergnaw <loren.supernaw@gmail.com>
2023-11-26 12:16:10 -0600

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
 if ! screen -ls | grep -Pq "[\d]+\.spigot"; then
 	cd /home/spigotmc/server
-	screen -dmS spigot java -Xmx2G -Xms2G -jar /home/spigot/server/spigot-1.20.2.jar nogui
+	screen -dmS spigot java -Xmx2G -Xms2G -jar /home/spigotmc/server/spigot-1.20.2.jar nogui
 fi
